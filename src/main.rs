9677a9abe35e11c17e71b81897a0b7e288cde921
Kenya <penta2himajin@protonmail.ch>
2019-07-02 01:07:49 +0900

    nyan

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 extern crate linalg_rs;
-
-use linalg_rs::{
-    Scalar,
-    Vector
-};
+use linalg_rs::{ Scalar, Vector };
 
 fn main() {
-    let s = Scalar::from(1);
+    let s = Scalar::from(2);
     println!("{}", s.unwrap());
     let v = Vector::from(vec![1, 2]);
     println!("{:?}", v.unwrap());
+    let s_mul_v = s*v;
+    println!("{:?}", s_mul_v.unwrap());
 }
