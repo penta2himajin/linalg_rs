9c9f8b84ce75a8b135f0bf065f76dad4f6588c30
NaraKenya <nara.kenya@unipro.co.jp>
2020-09-06 02:53:35 +0900

    add anotation to main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@ use linalg_rs::{ Scalar, Vector };
 
 fn main() {
     let s = Scalar::from(2);
-    println!("{}", s.unwrap());
+    println!("Scalar (2)   : {}", s.unwrap());
     let v_1 = Vector::from(vec![1, 2]);
-    println!("{:?}", v_1.unwrap());
+    println!("Vector [1, 2]: {:?}", v_1.unwrap());
     let s_mul_v1 = s * &v_1;
-    println!("{:?}", s_mul_v1.unwrap());
+    println!("(2) * [3, 4] : {:?}", s_mul_v1.unwrap());
     let v_2 = Vector::from(vec![3, 4]);
-    println!("{:?}", v_2.unwrap());
+    println!("Vector [3, 4]: {:?}", v_2.unwrap());
     let v1_add_v2 = &v_1 + &v_2;
-    println!("{:?}", v1_add_v2.unwrap());
+    println!("[1,2] + [3,4]: {:?}", v1_add_v2.unwrap());
     let v1_sub_v2 = &v_1 - &v_2;
-    println!("{:?}", v1_sub_v2.unwrap());
+    println!("[1,2] - [3,4]: {:?}", v1_sub_v2.unwrap());
     let v1_v2_scoler_product = &v_1 * &v_2;
-    println!("{}", v1_v2_scoler_product);
+    println!("[1,2] * [3,4]: {}", v1_v2_scoler_product);
 }
